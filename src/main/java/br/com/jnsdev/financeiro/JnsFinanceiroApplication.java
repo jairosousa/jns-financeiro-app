4589583f039f17b1849492f7e7d6e492914fe504
jaironsousa1 <jaironsousa@gmail.com>
2019-06-23 09:06:12 -0300

    Criação da aplicação

---
+++ src/main/java/br/com/jnsdev/financeiro/JnsFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.jnsdev.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnsFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnsFinanceiroApplication.class, args);
+	}
+
+}
