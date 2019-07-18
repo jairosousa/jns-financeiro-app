efc7ebe59fe6bf04be1ab6b9b85aeaff917af0a8
Jairo <jnsousa@stefanini.com>
2019-07-18 16:53:24 -0300

    Começo da criação da listagem Lançamento Despesa

---
+++ src/main/java/br/com/jnsdev/financeiro/repository/projection/LancamentoDespesaDTO.java
@@ -0,0 +1,43 @@
+package br.com.jnsdev.financeiro.repository.projection;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import br.com.jnsdev.financeiro.domain.Categoria;
+import br.com.jnsdev.financeiro.domain.FormaPagamento;
+import br.com.jnsdev.financeiro.domain.Fornecedor;
+import br.com.jnsdev.financeiro.domain.enuns.Pagamento;
+
+public interface LancamentoDespesaDTO {
+
+	Long getId();
+
+	String getNome();
+
+	String getDescricao();
+
+	BigDecimal getValor();
+
+	LocalDate getDtLacamento();
+
+	Fornecedor getFornecedor();
+
+	Categoria getCategoria();
+	
+	LocalDate getDtPagamento();
+	
+	LocalDate getDtVencimento();
+	
+	boolean getGastoFixo();
+	
+	Pagamento getPagamento();
+	
+	Integer getQtdParcelas();
+	
+	Integer getNumParcelas();
+	
+	BigDecimal getValorParcela();
+	
+	FormaPagamento getFormaPagamento();
+	
+}
