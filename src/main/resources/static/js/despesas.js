4d9d19e84cd754677b9afd0f4ac854227dbcbe5a
Jairo <jnsousa@stefanini.com>
2019-08-07 07:58:59 -0300

    Ajuste na exibição de despesas (falha no ajax na mudança de mês).

--- src/main/resources/static/js/despesas.js
+++ src/main/resources/static/js/despesas.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
     moment.locale('pt-BR');
     var mes = $('#mes').val();
     var ano = $('#ano').val();
@@ -6,7 +6,7 @@ $(document).ready(function () {
     $('#anoAtual').text(ano);
 
 
-    $("#proximo").on("click", function () {
+    $("#proximo").on("click", function() {
         mes = parseInt(mes) + 1;
         if (mes > 12) {
             ano = parseInt(ano) + 1;
@@ -18,11 +18,10 @@ $(document).ready(function () {
         }
 
         atualizarDespesas(mes, ano);
-
     });
 
-    $("#anterior").on("click", function () {
-        $(document).ready(function () {
+    $("#anterior").on("click", function() {
+        $(document).ready(function() {
             mes = parseInt(mes) - 1;
             if (mes < 1) {
                 ano = parseInt(ano) - 1;
@@ -34,9 +33,11 @@ $(document).ready(function () {
             }
 
             atualizarDespesas(mes, ano);
+            
         });
-
-        $("#voltarMesAtual").on("click", function () {
+    });
+    $("#voltarMesAtual").on("click", function() {
+        $(document).ready(function() {
             mes = new Date().getMonth() + 1;
             ano = new Date().getFullYear();
 
@@ -44,76 +45,74 @@ $(document).ready(function () {
             $('#mesAtual').text(mes);
 
             atualizarDespesas(mes, ano);
-        });
+        })
+    })
 
-        function atualizarDespesas(mes, ano) {
+    function atualizarDespesas(mes, ano) {
 
-            $.ajax({
-                method: "GET",
-                url: "/pagamentos/listar/ajax",
-                data: {
-                    mes: mes,
-                    ano: ano
-                },
-                beforeSend: function () {
-                    $(".despesa").fadeOut(0, function () {
-                        $(this).empty();
-                    });
-                },
-                success: function (response) {
+        $.ajax({
+            method: "GET",
+            url: "/pagamentos/listar/ajax/",
+            data: {
+                mes: mes,
+                ano: ano
+            },
+            beforeSend: function() {
+                $(".despesa").fadeOut(0, function() {
+                    $(this).empty();
+                });
+            },
+            success: function(response) {
 
-                    if (response.length > 2030) {
-                        $(".despesa").fadeIn(0, function () {
-                            $(this).append(response);
-                        })
-                    } else {
-                        $(".despesa").fadeIn(0, function () {
-                            $(this).append(`<div class="row h4 m-0 text-center text-success">
+                if (response.length > 2030) {
+                    $(".despesa").fadeIn(0, function() {
+                        $(this).append(response);
+                    })
+                } else {
+                    $(".despesa").fadeIn(0, function() {
+                        $(this).append(`<div class="row h4 m-0 text-center text-success">
                             <div class="col-md-12">
                     <ul><li>Não há despesa vencida</li></ul>
 
                         </div>
                     </div>`)
-                        });
-                    }
+                    });
+                }
+
+            },
+        });
+    }
+
+
+}); // Fim reader
+
+$(function() {
+    $(document).ready(function() {
+        $('.btn-editar').on('click', function(event) {
+            event.preventDefault();
+
+            var botaoEditar = $(event.currentTarget);
+            var id = botaoEditar.data('codigo');
+
+            $.ajax({
+                method: "GET",
+                url: "/pagamentos/despesa/edit/" + id,
+                beforeSend: function() {
+                    // Abrir Modal
+                    $("#editar-despesas-modal").modal('show');
 
                 },
+                success: function(data) {
+                    $('#form-edit-promo #edt_id').val(data.id);
+                    $('#form-edit-promo #nome').val(data.nome);
+                    $('#form-edit-promo #descricao').val(data.descricao);
+                    $('#form-edit-promo #valorParcela').val(data.valorParcela);
+                },
+                error: function() {
+                    alert("Ops... ocorreu um erro, tente mais tarde.");
+                }
             });
-        }
+        });
 
-
-    }); // Fim reader
-
-
-    // Editar o Pagamento da despesa
-    $(function () {
-        $(document).ready(function () {
-            $('.btn-editar').on('click', function (event) {
-                event.preventDefault();
-
-                var botaoEditar = $(event.currentTarget);
-                var id = botaoEditar.data('codigo');
-
-                $.ajax({
-                    method: "GET",
-                    url: "/pagamentos/despesa/edit/" + id,
-                    beforeSend: function () {
-                        // Abrir Modal
-                        $("#editar-despesas-modal").modal('show');
-
-                    },
-                    success: function (data) {
-                        $('#form-edit-promo #edt_id').val(data.id);
-                        $('#form-edit-promo #nome').val(data.nome);
-                        $('#form-edit-promo #descricao').val(data.descricao);
-                        $('#form-edit-promo #valorParcela').val(data.valorParcela);
-                    },
-                    error: function () {
-                        alert("Ops... ocorreu um erro, tente mais tarde.");
-                    }
-                });
-            });
-
-        })
     })
 });
