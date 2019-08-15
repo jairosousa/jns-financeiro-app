65ea7e08176825ba7f984fbf38747480927efbb2
jaironsousa1 <jaironsousa@gmail.com>
2019-08-15 18:45:39 -0300

    Ajustes Migrations

--- src/main/resources/db/migration/V07__criar_usuario_admin.sql
+++ src/main/resources/db/migration/V07__criar_usuario_admin.sql
@@ -1,4 +1,4 @@
-INSERT INTO usuarios(email, senha, ativo)
-values ('jnsdevs@gmail.com', '$2a$10$dIu/8uXEv8qYqAgpMl.ybuCqPwuejsNbi3o6LDPhjbQpxsb2AxzjG', 1);
-INSERT INTO usuarios_tem_perfis(usuario_id, perfil_id)
-values (1, 1);
+INSERT INTO usuarios
+    (email, senha, ativo)
+values
+    ('jnsdevs@gmail.com', '$2a$10$dIu/8uXEv8qYqAgpMl.ybuCqPwuejsNbi3o6LDPhjbQpxsb2AxzjG', 1);
---
+++ src/main/resources/db/migration/V08__criar_usuario_perfis.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuarios_tem_perfis(usuario_id, perfil_id)
+values (1, 1);
